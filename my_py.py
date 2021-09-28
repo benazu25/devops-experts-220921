60783c071db42f0e98c57c5a9b7a8a3fd4d4f79e
benazu25 <benazu25@gmail.com>
2021-09-28 22:58:21 +0300

    datetime update

--- my_py.py
+++ my_py.py
@@ -1,10 +1,2 @@
-from selenium import webdriver
-from time import sleep
-
-browser = webdriver.Chrome(executable_path="C:/Users/BenAzoulay/PycharmProjects/DevOpsCourse/Lesson 4/my_selenium"
-                                           "/chromedriver/chromedriver.exe")
-
-browser.get('http://www.walla.co.il')
-sleep(1)
-
-browser.close()
+from datetime import datetime
+print(datetime.now())
